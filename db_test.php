90eef2210f59de91fed68e84dd314a3ba12567a4
gavintapp <gavin@solonode.com>
2016-01-11 23:05:51 +1100

    Success connecting to db
    Connects to db and returns an array of results, displaying them in a
    html table. Also added html head info.

--- db_test.php
+++ db_test.php
@@ -3,11 +3,41 @@
 // Report all PHP errors (see changelog)
 error_reporting(E_ALL);
 
-echo "DB test";
+$html_head = <<<EOD
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
 
-$mysqli = mysqli_connect("tower", "user", "password", "database");
-$res = mysqli_query($mysqli, "SELECT 'Please, do not use ' AS _msg FROM DUAL");
-$row = mysqli_fetch_assoc($res);
-echo $row['_msg'];
+EOD;
+
+echo $html_head;
+echo "<h1>DB test</h1>";
+
+$db_server = "192.168.1.35";
+$db_user = "temp";
+$db_pass = "temp";
+$db_name = "temp";
+
+
+$mysqli = mysqli_connect($db_server, $db_user, $db_pass, $db_name) or die(mysqli_error($mysqli));
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+$res = mysqli_query($mysqli, "SELECT * from sensor_log");
+
+
+echo "<table>\n";
+
+while ($row = mysqli_fetch_array($res)){
+	printf ("<tr><td>".$row["id"]."</td><td>".$row["timestamp"]."</td><td>".$row["temp"]."</td><td>".$row["humidity"]."</td><td>".$row["sensor_id"]."</td></tr>\n");
+}
+
+
+echo "</table>";
 
 ?>
